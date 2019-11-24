94cd5a7958ca7038b9b7033343c761eb6f966243
hoge-fuga-piyo <nao07770@yahoo.co.jp>
2019-11-24 12:51:21 +0900

    adapt PR diff

--- src/content/content.js
+++ src/content/content.js
@@ -2,29 +2,54 @@ function isMaximization() {
   const key = "isMaximization";
   return new Promise((resolve, reject) => {
     chrome.storage.local.get([key], (value) => {
-      //console.log(value[key]);
       resolve(value[key]);
     });
   })
 }
 
 function paneMaximization() {
-  if($('div').hasClass('repository-content')) {
+  if ($('div').hasClass('repository-content')) {
     console.log("has repository-content");
-    $('div.container-lg.clearfix.new-discussion-timeline.experiment-repo-nav.p-responsive').css('margin-left', '2%');
-    $('div.container-lg.clearfix.new-discussion-timeline.experiment-repo-nav.p-responsive').css('margin-right', '2%');
-    $('div.container-lg.clearfix.new-discussion-timeline.experiment-repo-nav.p-responsive').css('max-width', '100%');
-    $('div.repository-content').width('auto');
+
+    // ソースコード表示
+    codePaneMaximization();
+
+    // PRのdiff表示
+    prDiffPaneMaximization();
   }
 }
 
 function paneDefault() {
-  if($('div').hasClass('repository-content')) {
+  if ($('div').hasClass('repository-content')) {
     console.log("has repository-content");
-    $('div.container-lg.clearfix.new-discussion-timeline.experiment-repo-nav.p-responsive').css('margin-left', 'auto');
-    $('div.container-lg.clearfix.new-discussion-timeline.experiment-repo-nav.p-responsive').css('margin-right', 'auto');
-    $('div.container-lg.clearfix.new-discussion-timeline.experiment-repo-nav.p-responsive').css('max-width', '1012px');
+    codePaneDefault();
+    prDiffPaneDefault();
   }
+}
+
+function codePaneMaximization() {
+  $('div.container-lg.clearfix.new-discussion-timeline.experiment-repo-nav.p-responsive').css('margin-left', '2%');
+  $('div.container-lg.clearfix.new-discussion-timeline.experiment-repo-nav.p-responsive').css('margin-right', '2%');
+  $('div.container-lg.clearfix.new-discussion-timeline.experiment-repo-nav.p-responsive').css('max-width', '100%');
+  $('div.repository-content').width('auto');
+}
+
+function codePaneDefault() {
+  $('div.container-lg.clearfix.new-discussion-timeline.experiment-repo-nav.p-responsive').css('margin-left', 'auto');
+  $('div.container-lg.clearfix.new-discussion-timeline.experiment-repo-nav.p-responsive').css('margin-right', 'auto');
+  $('div.container-lg.clearfix.new-discussion-timeline.experiment-repo-nav.p-responsive').css('max-width', '1012px');
+}
+
+function prDiffPaneMaximization() {
+  $('div.container-lg.clearfix.new-discussion-timeline.experiment-repo-nav.px-3').css('margin-left', '2%');
+  $('div.container-lg.clearfix.new-discussion-timeline.experiment-repo-nav.px-3').css('margin-right', '2%');
+  $('div.container-lg.clearfix.new-discussion-timeline.experiment-repo-nav.px-3').css('max-width', '100%');
+}
+
+function prDiffPaneDefault() {
+  $('div.container-lg.clearfix.new-discussion-timeline.experiment-repo-nav.px-3').css('margin-left', 'auto');
+  $('div.container-lg.clearfix.new-discussion-timeline.experiment-repo-nav.px-3').css('margin-right', 'auto');
+  $('div.container-lg.clearfix.new-discussion-timeline.experiment-repo-nav.px-3').css('max-width', '1012px');
 }
 
 function runScript() {
@@ -40,6 +65,7 @@ function runScript() {
   })
 }
 
+// 拡張機能ボタンが押された時の挙動
 chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
     if (request == "runScript") {
         runScript();
